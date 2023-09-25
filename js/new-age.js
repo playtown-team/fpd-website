3cc6d34d7df7a9d537bb4f4dde041b5094d103ab
EDGAR VERA <orurando@192.168.1.4>
2023-09-25 12:24:37 -0300

    probando volver a subir cambios sin ejecutar bulk

--- js/new-age.js
+++ js/new-age.js
@@ -28,4 +28,24 @@
         }
     })
 
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        $('.btn-android').show();
+        $('.btn-ios').show();
+
+        if (/android/i.test(userAgent)) {
+            $('.btn-android').show();
+            $('.btn-ios').hide();
+        }
+    
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            $('.btn-android').hide();
+            $('.btn-ios').show();
+        }
+           
+    }
+    getMobileOperatingSystem();
+
+
 })(jQuery); // End of use strict
